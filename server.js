428f986fc57b6bb288aa77f3021e12d33984a18a
gautam bhuyan <gautam@gautams-MacBook-Pro.local>
2016-10-09 23:03:49 -0400

    Updated

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ var express = require('express'),
     fs      = require('fs'),
     app     = express(),
     eps     = require('ejs'),
-    morgan  = require('morgan');
+    morgan  = require('morgan'),
+    lib     = require('body-parser'),
+    dbapp   = require('./db.js');
     
 Object.assign=require('object-assign')
 
@@ -99,6 +101,70 @@ app.use(function(err, req, res, next){
 
 initDb(function(err){
   console.log('Error connecting to Mongo. Message:\n'+err);
+});
+
+app.use(lib.json());
+app.use(lib.urlencoded({extended:true}));
+
+app.get('/interests', function(req, res) {
+    var response = function(data) {
+        res.json(data);
+    }
+        
+    dbapp.getInterests(db, response);
+});
+    
+app.get('/activities/interest/:interest/lat/:lat/lng/:lng', function(req, res) {
+    var interest = req.params.interest;
+    var lat = req.params.lat;
+    var lng = req.params.lng;
+    var response = function(data) {
+        res.json(data);
+    }
+    dbapp.getActivities(db, interest, lat, lng, response);
+});
+    
+app.get('/messages/activity/:activity', function(req, res) {
+    var activity = req.params.activity;
+    //console.log(activity);
+    var response = function(data) {
+        res.json(data);
+    }
+    dbapp.getMessages(db, activity, response);
+});
+    
+app.post('/interest', function(req, res) {
+    var interest = req.body.interest;
+    dbapp.addInterest(db, interest);
+    console.log({interest:interest});
+    res.json({done:true});
+});
+    
+app.post('/activity', function(req, res) {
+    var interest = req.body.interest,
+        lat = req.body.lat,
+        lng = req.body.lng,
+        activity = req.body.activity;
+    console.log({interest:interest, lat:lat, lng:lng});
+    dbapp.createActivity(db, interest, activity, lat, lng);
+    res.json({done:true});
+});
+    
+app.post('/message', function(req, res) {
+    var activity = req.body.activity,
+        message = req.body.message;
+    console.log({activity:activity, message:message});
+    res.json({done:true});
+});
+    
+app.use(function(req, res, next) {
+    res.writeHead(200, {'Content-type' : 'text/html'});
+    res.write("<h1> Hello world <\h1>");
+    next();
+});
+
+app.use(function(req, res) {
+    res.end("<h2> Bye bye world<\h2>");
 });
 
 app.listen(port, ip);
